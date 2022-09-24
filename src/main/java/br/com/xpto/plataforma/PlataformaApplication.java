d19b77546fd26ff9188b6ae1a0c7a4c2b6e3200c
Caroline Tavares <carolinetvrs@outlook.com>
2022-09-24 17:46:28 -0300

    Ajuste nome da empresa

--- src/main/java/br/com/xpto/plataforma/PlataformaApplication.java
+++ src/main/java/br/com/xpto/plataforma/PlataformaApplication.java
@@ -1,4 +1,4 @@
-package br.com.cravoecanela.plataforma;
+package br.com.xpto.plataforma;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/br/com/xpto/plataforma/PlataformaApplicationTests.java
+++ src/test/java/br/com/xpto/plataforma/PlataformaApplicationTests.java
@@ -1,4 +1,4 @@
-package br.com.cravoecanela.plataforma;
+package br.com.xpto.plataforma;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
