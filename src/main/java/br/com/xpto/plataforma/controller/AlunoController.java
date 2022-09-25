6ce51dc121d8ab6a7c8e182d607358f10913575c
Caroline Tavares <carolinetvrs@outlook.com>
2022-09-25 02:01:20 -0300

    Endpoints GET aluno e POST aluno ok

--- src/main/java/br/com/xpto/plataforma/controller/AlunoController.java
+++ src/main/java/br/com/xpto/plataforma/controller/AlunoController.java
@@ -1,8 +1,42 @@
 package br.com.xpto.plataforma.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import br.com.xpto.plataforma.model.Aluno;
+import br.com.xpto.plataforma.service.IAlunoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @RestController
 public class AlunoController {
+
+    @Autowired
+    private IAlunoService service;
+
+    @GetMapping("/aluno")
+    public ArrayList<Aluno> recuperarTodos(){
+        return service.buscarTodos();
+    }
+
+    @GetMapping("/aluno/{cpf}")
+    public ResponseEntity<Aluno> retornarAlunoCpf(@PathVariable String cpf){
+        return null;
+    }
+
+    @PostMapping("/aluno")
+    public ResponseEntity<Aluno> cadastrarAluno(@RequestBody Aluno aluno){
+        Aluno res = service.cadastrarNovoAluno(aluno);
+        if (res != null){
+            return ResponseEntity.ok(res);
+        }
+            return ResponseEntity.badRequest().build();
+    }
+
+    @DeleteMapping("/aluno/{id}")
+    public ResponseEntity<Aluno> excluirAluno(@PathVariable Integer id){
+        service.excluirAluno(id);
+        return null;
+    }
 
 }
--- src/main/java/br/com/xpto/plataforma/service/AlunoServiceImpl.java
+++ src/main/java/br/com/xpto/plataforma/service/AlunoServiceImpl.java
@@ -1,8 +1,49 @@
 package br.com.xpto.plataforma.service;
 
+import br.com.xpto.plataforma.dao.AlunoDAO;
+import br.com.xpto.plataforma.model.Aluno;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 
 @Service
 public class AlunoServiceImpl implements IAlunoService {
 
+    @Autowired
+    private AlunoDAO dao;
+
+    @Override
+    public Aluno cadastrarNovoAluno(Aluno novo) {
+        if (!novo.getNome().isEmpty() && !novo.getSobrenome().isEmpty() && !novo.getCpf().isEmpty() && !novo.getCelular().isEmpty()
+                && !novo.getEndereco().isEmpty() && !novo.getEmail().isEmpty()) {
+            return dao.save(novo);
+        }
+            return null;
+    }
+
+    @Override
+    public Aluno atualizarDados(Aluno dados) {
+        return null;
+    }
+
+    @Override
+    public ArrayList<Aluno> buscarTodos() {
+        return (ArrayList<Aluno>) dao.findAll();
+    }
+
+    @Override
+    public Aluno buscarPeloId(Integer id) {
+        return dao.findById(id).orElse(null);
+    }
+
+    @Override
+    public Aluno buscarCpf(String cpf) {
+        return null;
+    }
+
+    @Override
+    public void excluirAluno(Integer id) {
+        dao.deleteById(id);
+    }
 }
