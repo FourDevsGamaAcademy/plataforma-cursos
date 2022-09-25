f934bf5cd7c981b609d63e60e949c2732ea36c39
RaphaelaCristina <raphitasp@msn.com>
2022-09-25 20:40:58 -0300

    corrigindo model empresa

---
+++ src/main/java/br/com/xpto/plataforma/controller/EmpresaController.java
@@ -0,0 +1,25 @@
+package br.com.xpto.plataforma.controller;
+
+import br.com.xpto.plataforma.dao.EmpresaDAO;
+import br.com.xpto.plataforma.model.Empresa;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+
+
+public class EmpresaController {
+
+    @Autowired
+  private EmpresaDAO dao;
+
+    @GetMapping("/empresa")
+    public ArrayList<Empresa> recuperarTodas(){
+        ArrayList<Empresa> lista;
+        lista = (ArrayList<Empresa>) dao.findAll();
+        return  lista;
+    }
+
+
+}
---
+++ src/main/java/br/com/xpto/plataforma/dao/EmpresaDAO.java
@@ -0,0 +1,2 @@
+package br.com.xpto.plataforma.dao;public interface EmpresaDAO {
+}
