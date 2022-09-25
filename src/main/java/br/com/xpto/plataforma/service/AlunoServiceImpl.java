68ecefb486f497321be3cdae1b149556c2417e10
Caroline Tavares <carolinetvrs@outlook.com>
2022-09-25 00:16:23 -0300

    Ajuste no nome dos arquivos

--- src/main/java/br/com/xpto/plataforma/service/AlunoServiceImpl.java
+++ src/main/java/br/com/xpto/plataforma/service/AlunoServiceImpl.java
@@ -3,6 +3,6 @@ package br.com.xpto.plataforma.service;
 import org.springframework.stereotype.Service;
 
 @Service
-public class AlunaServiceImpl implements IAlunaService{
+public class AlunoServiceImpl implements IAlunoService {
 
 }
--- src/main/java/br/com/xpto/plataforma/service/IAlunoService.java
+++ src/main/java/br/com/xpto/plataforma/service/IAlunoService.java
@@ -1,5 +1,5 @@
 package br.com.xpto.plataforma.service;
 
-public interface IAlunaService {
+public interface IAlunoService {
 
 }
